db322d50d1ab7981167d3e4ecf85b28d98eb7749
EricaManisOliveira <109998422+EricaManisOliveira@users.noreply.github.com>
2022-09-16 21:55:01 -0300

    DML-Inserts.sql
    Criação de 15 itens na tabela Produto

---
+++ DML-Inserts.sql
@@ -0,0 +1,18 @@
+use vitrine_erica;
+
+insert into produto(nomeProduto,valorProduto,descricaoProduto,estoqueProduto,departamentoProduto,categoriaProduto)
+values("Camiseta Branca", 89.90," Camiseta básica branca", 15, "Feminino","Camiseta"),
+("Regata Preta", 29.90," Regata básica preta", 10, "Feminino","Regata"),
+("Calça Preta", 129.90," Calça jeans preta ", 20, "Feminino","Calça"),
+("Short Rosa", 69.90," Short jeans rosa", 21, "Infantil","Short"),
+("Blusao moletom", 119.90," Blusao moletom basico", 25, "Masculino","Moletom"),
+("Top malha", 39.90," Top esportivo básico", 36, "Feminino","Top"),
+("Camiseta manga longa", 69.90 ," Camiseta básica manga longa", 11, "Feminino","Camiseta"),
+("Calça moletom", 89.90," Calça moletom básica", 29, "Infantil","Calça"),
+("Cueca boxer", 25.90," Cueca algodão boxer", 13, "Masculino","Cueca"),
+("Maio adulto", 169.90," Maio adulto pluz size", 30, "Feminino","Maio"),
+("Sunga infantil", 59.90," Sunga infantil azul", 22, "Infantil","Sunga"),
+("Sutia feminino", 49.90," Sutia feminino adulto preto", 17, "Feminino","Sutia"),
+("Biquini infantil", 69.90," Biquini infantil rosa", 26, "Infantil","Biquini"),
+("Meia Branca", 10.90," Meia infantil branca", 20, "Infantil","Meia"),
+("Saia verde", 99.90," Saia longa verde", 22, "Feminino","Saia");
