4325347c96b74dbc98e156f4aecf6b72cf3f4962
EricaManisOliveira <109998422+EricaManisOliveira@users.noreply.github.com>
2022-09-16 21:58:11 -0300

    DML-Updates.sql
    Updates itens tabela Produto

---
+++ DML-Updates.sql
@@ -0,0 +1,33 @@
+use vitrine_erica;
+
+UPDATE produto 
+SET 
+    valorProduto = 119.90
+WHERE
+    idProduto = 4;
+
+UPDATE produto 
+SET 
+    valorProduto = 29.90
+WHERE
+    idProduto = 5;
+
+UPDATE produto 
+SET 
+    estoqueProduto = 9
+WHERE
+    idProduto = 2;
+
+UPDATE produto 
+SET 
+    descricaoProduto = 'Regata básica branca'
+WHERE
+    idProduto = 2
+
+
+
+
+
+
+
+
